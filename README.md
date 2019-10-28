8ae119f6c9d27031dacf9fae6ac9444244adc017
Hubert Yang <offspring7890@yahoo.com>
2019-10-28 12:57:38 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Noteful Client
+Deploye here https://nandana-noteful-client.now.sh
---
+++ src/config.js
@@ -0,0 +1,4 @@
+export default {
+  API_ENDPOINT: 'https://afternoon-cove-86140.herokuapp.com/api',
+  API_KEY: process.env.REACT_APP_API_KEY,
+}
